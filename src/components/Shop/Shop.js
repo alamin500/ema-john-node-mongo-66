666f09711fd9fd6146f537eab0dc9cbbf4e55143
alamin500 <nalamin500@gmail.com>
2021-10-26 15:08:43 +0600

    j

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -13,18 +13,18 @@ const Shop = () => {
   const [pageCount, setPageCount] = useState(0);
   // products to be rendered on the UI
   const [displayProducts, setDisplayProducts] = useState([]);
-
+  const size = 10;
   useEffect(() => {
-    fetch("http://localhost:5000/products")
+    fetch(`http://localhost:5000/products?page=${page}&&size=${size}`)
       .then((res) => res.json())
       .then((data) => {
         setProducts(data.products);
         setDisplayProducts(data.products);
         const count = data.count;
-        const pageNumber = Math.ceil(count / 10);
+        const pageNumber = Math.ceil(count / size);
         setPageCount(pageNumber);
       });
-  }, []);
+  }, [page]);
 
   const handleAddToCart = (product) => {
     const exists = cart.find((pd) => pd.key === product.key);
@@ -77,7 +77,7 @@ const Shop = () => {
                 key={number}
                 onClick={() => setPage(number)}
               >
-                {number}
+                {number + 1}
               </button>
             ))}
           </div>
--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,29 +1,27 @@
-import { useState, useEffect } from 'react';
-import { getStoredCart } from '../utilities/fakedb';
+import { useState, useEffect } from "react";
+import { getStoredCart } from "../utilities/fakedb";
 
-const useCart = products => {
-    const [cart, setCart] = useState([]);
+const useCart = (products) => {
+  const [cart, setCart] = useState([]);
 
-    useEffect(() => {
-
-        if (products.length) {
-            const savedCart = getStoredCart();
-            const storedCart = [];
-            for (const key in savedCart) {
-                const addedProduct = products.find(product => product.key === key);
-                if (addedProduct) {
-                    // set quantity
-                    const quantity = savedCart[key];
-                    addedProduct.quantity = quantity;
-                    storedCart.push(addedProduct);
-                }
-            }
-            setCart(storedCart);
+  useEffect(() => {
+    if (products?.length) {
+      const savedCart = getStoredCart();
+      const storedCart = [];
+      for (const key in savedCart) {
+        const addedProduct = products.find((product) => product.key === key);
+        if (addedProduct) {
+          // set quantity
+          const quantity = savedCart[key];
+          addedProduct.quantity = quantity;
+          storedCart.push(addedProduct);
         }
+      }
+      setCart(storedCart);
+    }
+  }, [products]);
 
-    }, [products]);
-
-    return [cart, setCart];
-}
+  return [cart, setCart];
+};
 
 export default useCart;
